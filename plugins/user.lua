df772442323a7a51e128a95ac886db480f5feb7b
Chris <c.rausch020@gmail.com>
2023-11-30 11:48:20 +0100

    Adds config

--- plugins/user.lua
+++ plugins/user.lua
@@ -38,6 +38,8 @@ return {
   },
   {
     "kylechui/nvim-surround",
+    version = "*",
+    event = "VeryLazy",
     config = function()
       require("nvim-surround").setup {
         -- Configuration here, or leave empty to use defaults
